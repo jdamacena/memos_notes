5ed5ea8c7cfaa775bcf89c906d8e91f0df886aa2
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2023-02-14 20:09:19 -0300

    Changed note item style

--- lib/widgets/note_list_item.dart
+++ lib/widgets/note_list_item.dart
@@ -13,37 +13,77 @@ class NoteListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 2.0),
-      margin: EdgeInsets.only(
-        top: 8.0,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(blurRadius: 3.0, color: Colors.grey),
-        ],
-        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-      ),
-      child: ListTile(
-        leading: Column(mainAxisAlignment: MainAxisAlignment.center,
+    const textPadding = const EdgeInsets.all(2.0);
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(4.0),//symmetric(vertical: 2.0),
+        margin: EdgeInsets.only(
+          top: 8.0,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(blurRadius: 3.0, color: Colors.grey),
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+        ),
+        child: Row(
           children: [
-            Icon(note.archived ? Icons.archive_outlined : Icons.edit_note),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(note.archived ? Icons.archive_outlined : Icons.edit_note),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: textPadding,
+                    child: Text(
+                      note.title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  if (note.description.trim().length > 0)
+                    Padding(
+                      padding: textPadding,
+                      child: Text(
+                        note.description,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  Padding(
+                    padding: textPadding,
+                    child: Text(
+                      DateTime.fromMillisecondsSinceEpoch(
+                        int.parse(note.timestamp)).toString(),
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-        title: Text(
-          note.title,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: note.description.trim().length <= 0
-            ? null
-            : Text(
-                note.description,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-        onTap: onTap,
       ),
     );
   }
