f8db211ca057f78ec86fe57d31ffdf7a52e26a49
Junior Damacena <19500428+jdamacena@users.noreply.github.com>
2023-04-15 00:46:21 -0300

    Workaround for problem that the app preview on the recent apps screen on Motorola phone shows flickering and a black screen

--- android/app/src/main/kotlin/com/juniordamacena/notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/juniordamacena/notes/MainActivity.kt
@@ -3,4 +3,16 @@ package com.juniordamacena.notes
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    protected override fun onPause() {
+        super.onPause()
+
+        // Workaround for problem that the app preview on the recent apps
+        // screen on Motorola phone shows flickering and a black screen, more details here:
+        // https://github.com/flutter/flutter/issues/66212
+        try {
+            java.lang.Thread.sleep(200)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
 }
